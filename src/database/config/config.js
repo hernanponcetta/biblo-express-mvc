a9a7f4ce27f7347b0d62dca046ba99aa2ab7f736
HernanP <hernanponcetta@gmail.com>
2021-06-13 10:39:55 +0200

    Remote db added

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,27 +1,29 @@
+const config = require('config');
+
 module.exports = {
   development: {
-    username: "root",
-    password: "dalepincha",
-    database: "biblo_db",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    username: 'admin',
+    password: config.get('dbConfig.password'),
+    database: 'biblo-database',
+    host: 'biblo-database.cf56unoyaeaf.us-east-1.rds.amazonaws.com',
+    dialect: 'mysql',
     define: {
       underscored: true,
       paranoid: true,
     },
   },
   test: {
-    username: "root",
-    password: "root",
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    username: 'root',
+    password: 'root',
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
   },
   production: {
-    username: "root",
-    password: "root",
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    username: 'root',
+    password: 'root',
+    database: 'database_production',
+    host: '127.0.0.1',
+    dialect: 'mysql',
   },
 };
