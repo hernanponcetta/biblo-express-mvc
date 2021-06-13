c3415a72f783a8738b2d97070e40987cea77cf4f
HernanP <hernanponcetta@gmail.com>
2021-06-13 16:02:26 +0200

    db config updated

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -13,17 +13,25 @@ module.exports = {
     },
   },
   test: {
-    username: 'root',
-    password: 'root',
-    database: 'database_test',
-    host: '127.0.0.1',
+    username: 'admin',
+    password: config.get('dbConfig.password'),
+    database: 'biblo-database',
+    host: 'biblo-database.cf56unoyaeaf.us-east-1.rds.amazonaws.com',
     dialect: 'mysql',
+    define: {
+      underscored: true,
+      paranoid: true,
+    },
   },
   production: {
-    username: 'root',
-    password: 'root',
-    database: 'database_production',
-    host: '127.0.0.1',
+    username: 'admin',
+    password: config.get('dbConfig.password'),
+    database: 'biblo-database',
+    host: 'biblo-database.cf56unoyaeaf.us-east-1.rds.amazonaws.com',
     dialect: 'mysql',
+    define: {
+      underscored: true,
+      paranoid: true,
+    },
   },
 };
